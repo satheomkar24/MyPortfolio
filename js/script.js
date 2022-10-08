fda87f3d850a39cf19d54b2f300fed19b1f92bb1
omkarsathe <satheomkar143@gmail.com>
2022-10-08 20:34:49 +0530

    various color theme added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// typing animation
+
+var typed = new Typed(".typing",{
+  strings:["","Web Developer",  "Graphics Designer", " Web Designer", "Java Developer", "Full-stack Java Developer"],
+  typeSpeed:100,
+  BackSpeed:60,
+  loop:true
+}
+)
